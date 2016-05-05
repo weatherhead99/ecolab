13eac0605da10ae4b37972f6f6ff0e10ea48fb91
Dan Weatherill <danw@linux-thinkpad.site>
2016-05-05 15:06:11 +0100

    starting.. generate_nauty_sizes done

---
+++ generate_nauty_sizes.cmake
@@ -0,0 +1,8 @@
+include(CheckTypeSize)
+
+message(STATUS "generating nauty_sizes.h")
+check_type_size(int SIZEOF_INT)
+check_type_size(long SIZEOF_LONG)
+check_type_size("long long" SIZEOF_LONG_LONG)
+
+configure_file(nauty_sizes.h.in nauty_sizes.h)
